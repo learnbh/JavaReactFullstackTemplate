851f45b9984907537a93ab00387e7615f2d9beb1
learnjavabh <learnmsbh@outlook.de>
2025-05-12 20:08:18 +0200

    fixed problem because maven was missing

--- backend/src/main/java/org/example/backend/controller/PullExmpleController.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.web.bind.annotiation.RequestMapping;
-import org.springframework.web.bind.annotiation.RestController;
-
-@RestController
-@RequqstMapping("api/pullExample")
-public class PullExmpleController {
-
-}
