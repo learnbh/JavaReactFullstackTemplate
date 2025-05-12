eb05f14c765462a47296fa45997ab66690f055ae
learnjavabh <learnmsbh@outlook.de>
2025-05-12 11:50:51 +0200

    add pullexample controller

---
+++ backend/src/main/java/org/example/backend/controller/PullExmpleController.java
@@ -0,0 +1,8 @@
+import org.springframework.web.bind.annotiation.RequestMapping;
+import org.springframework.web.bind.annotiation.RestController;
+
+@RestController
+@RequqstMapping("api/pullExample")
+public class PullExmpleController {
+
+}
